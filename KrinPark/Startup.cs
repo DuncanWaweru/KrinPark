f29ca3a0391934834f7efb6dddffff1a00d34a8a
administrator <dancanwaweru01@hotmail.com>
2021-09-11 22:15:17 +0300

    Add project files.

---
+++ KrinPark/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KrinPark.Startup))]
+namespace KrinPark
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
